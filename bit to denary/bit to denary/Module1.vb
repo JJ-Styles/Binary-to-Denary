52f166ecbbe923df6f3e43db030697ba0b782c83
Jason Flower <35689497+JJ-Styles@users.noreply.github.com>
2019-11-03 20:30:26 +0000

    Project Upload

---
+++ bit to denary/bit to denary/Module1.vb
@@ -0,0 +1,22 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim answer As Integer = 0
+        Dim bit As String
+        Dim column As Integer = 0
+
+        Console.WriteLine("Enter Bit Value:")
+        bit = Console.ReadLine
+        column = Math.Pow(2, (bit.ToString.Length - 1))
+        answer = answer + (column * Convert.ToInt32(bit.ToString.Substring(0, 1)))
+
+        Do
+            bit = bit.ToString.Substring(1, (bit.ToString.Length - 1))
+            column = column / 2
+            answer = answer + (column * Convert.ToInt32(bit.ToString.Substring(0, 1)))
+        Loop Until (column < 1) Or (bit.ToString.Length = 1)
+        Console.WriteLine("Decimal Value Is: " & answer)
+        Console.ReadKey()
+    End Sub
+
+End Module
